44c0afa3e1da6515926a04835a1fbb4d0524fbf8
suhas1294 <suhasr94@gmail.com>
2021-12-16 13:15:05 +0530

    reuses util

--- src/main/java/rishi/atreya/_01_arrays/_01_ReverseArray.java
+++ src/main/java/rishi/atreya/_01_arrays/_01_ReverseArray.java
@@ -1,31 +1,28 @@
 package rishi.atreya._01_arrays;
 
+import rishi.atreya._099_utils.Util;
+
 public class _01_ReverseArray {
 	public static void main(String[] args) {
 		int[] input = {1,2,3,4,5};
 
 		int start = 0;
 		int end = input.length -1 ; // -1 because 0 based indexing
-		int temp;
 
 		while (start < end){
-			temp = input[start];
-			input[start] = input[end];
-			input[end] = temp;
+			Util.swapIndicesOfanArray(input, start, end);
 			start++;
 			end--;
 		}
 
 		for (int j : input) { System.out.print(j + "\t"); }
 	}
-	// recursive way
+
+	// alternate solution using recursive way
 	static void reverseArray(int[] arr, int start, int end){
-		int temp;
 		if (start >= end)
 			return;
-		temp = arr[start];
-		arr[start] = arr[end];
-		arr[end] = temp;
+		Util.swapIndicesOfanArray(arr, start, end);
 		reverseArray(arr, start+1, end-1);
 	}
 }
